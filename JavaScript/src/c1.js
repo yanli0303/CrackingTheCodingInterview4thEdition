1a5eebb7ed056a816a5f22bdabffc8010d3d0f97
Li, Yan <peterleepersonal@gmail.com>
2015-03-11 18:37:55 +0800

    Chapter 1 done.

--- JavaScript/src/c1.js
+++ JavaScript/src/c1.js
@@ -1,7 +1,13 @@
 var c1 = {
-    isStringHasAllUniqueCharacters: function(str) {
+    // Implement an algorithm to determine if a string has all unique characters.
+    // What if you can not use additional data structures?
+    isStringHasAllUniqueCharacters: function (str) {
         if (typeof str !== 'string') {
-            throw new Error('Invalid argument: "str" should be a string.');
+            return false;
+        }
+
+        if (!str) {
+            return true;
         }
 
         var hashTable = {},
@@ -21,9 +27,11 @@ var c1 = {
         return true;
     },
 
-    reverseString: function(str) {
-        if (typeof str !== 'string') {
-            throw new Error('Invalid argument: "str" should be a string.');
+    // Write code to reverse a C-Style String 
+    // (C-String means that ¡°abcd¡± is represented as five characters, including the null character )
+    reverseString: function (str) {
+        if (typeof str !== 'string' || !str) {
+            return str;
         }
 
         var reversedArray = [],
@@ -37,12 +45,11 @@ var c1 = {
         return reversedArray.join('');
     },
 
-    removeDuplicateCharactersWithoutAdditionalMemory: function(str) {
-        if (typeof str !== 'string') {
-            throw new Error('Invalid argument: "str" should be a string.');
-        }
-
-        if (str.length < 2) {
+    // Design an algorithm and write code to remove the duplicate characters in a string
+    // without using any additional buffer 
+    // NOTE: One or two additional variables are fine, an extra copy of the array is not
+    removeDuplicateCharactersWithoutAdditionalMemory: function (str) {
+        if (typeof str !== 'string' || str.length < 2) {
             return str;
         }
 
@@ -67,13 +74,8 @@ var c1 = {
 
         return resultArray.join('');
     },
-
-    removeDuplicateCharactersHashTable: function(str) {
-        if (typeof str !== 'string') {
-            throw new Error('Invalid argument: "str" should be a string.');
-        }
-
-        if (str.length < 2) {
+    removeDuplicateCharactersHashTable: function (str) {
+        if (typeof str !== 'string' || str.length < 2) {
             return str;
         }
 
@@ -92,5 +94,128 @@ var c1 = {
         }
 
         return resultArray.join('');
+    },
+
+    // Write a method to decide if two strings are anagrams or not
+    areAnagramsXor: function (str1, str2) {
+        if (typeof str1 !== 'string' || typeof str2 !== 'string' || !str1 || str1.length !== str2.length) {
+            return false;
+        }
+
+        var xor = 0,
+            i;
+
+        for (i = str1.length - 1; i >= 0; i -= 1) {
+            xor ^= str1.charCodeAt(i);
+        }
+
+        for (i = str2.length - 1; i >= 0; i -= 1) {
+            xor ^= str2.charCodeAt(i);
+        }
+
+        return xor === 0;
+    },
+
+    // Write a method to replace all spaces in a string with "%20"
+    replaceWhiteSpaceWith: function (str, replacement) {
+        if (typeof str !== 'string' || !str || typeof replacement !== 'string' || !replacement) {
+            return str;
+        }
+
+        return str.replace(/\s/g, replacement);
+    },
+
+    // Given an image represented by an NxN matrix, where each pixel in the image is 4
+    // bytes, write a method to rotate the image by 90 degrees Can you do this in place?
+    rotateMatrix90Degrees: function (matrix) {
+        if (!matrix || !matrix.length) {
+            return matrix;
+        }
+
+        var rows = matrix.length,
+            columns,
+            rotatedMatrix = [],
+            row,
+            ri,
+            ci;
+
+        // a plain array as in [1, 2, 3]
+        if (!matrix[0].length) {
+            for (var i = 0, e; i < rows; i += 1) {
+                e = matrix[i];
+                rotatedMatrix[i] = [e];
+            }
+
+            return rotatedMatrix;
+        }
+
+        // matrix
+        columns = matrix[0].length;
+        for (ri = rows - 1; ri >= 0; ri -= 1) {
+            row = matrix[ri];
+            for (ci = 0; ci < columns; ci += 1) {
+                if (ri === rows - 1) {
+                    rotatedMatrix[ci] = [row[ci]];
+                } else {
+                    rotatedMatrix[ci].push(row[ci]);
+                }
+            }
+        }
+
+        return rotatedMatrix;
+    },
+
+    // Write an algorithm such that if an element in an MxN matrix is 0, 
+    // its entire row and column is set to 0
+    setMatrixZero: function (matrix) {
+        if (!matrix || !matrix.length || !matrix[0].length) {
+            return matrix;
+        }
+
+        var rows = matrix.length,
+            columns = matrix[0].length,
+            rowZeroPositions = [],
+            columnZeroPositions = [],
+            ri,
+            ci;
+
+        for (ri = 0; ri < rows; ri += 1) {
+            for (ci = 0; ci < columns; ci += 1) {
+                if (matrix[ri][ci] === 0) {
+                    rowZeroPositions[ri] = 1;
+                    columnZeroPositions[ci] = 1;
+                }
+            }
+        }
+
+        for (ri = 0; ri < rows; ri += 1) {
+            for (ci = 0; ci < columns; ci += 1) {
+                if (rowZeroPositions[ri] === 1 || columnZeroPositions[ci] === 1) {
+                    matrix[ri][ci] = 0;
+                }
+            }
+        }
+
+        return matrix;
+    },
+
+    // Assume you have a method isSubstring which checks if one word is a substring of
+    // another Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using
+    // only one call to isSubstring (i e , "waterbottle" is a rotation of "erbottlewat")
+    isRotationWith1CallToIsSubstring: function (str1, str2) {
+        if (typeof str1 !== 'string' || typeof str2 !== 'string' || !str1 || str1.length !== str2.length) {
+            return false;
+        }
+
+        // indexOf is the isSubstring function
+        return (str1 + str1).indexOf(str2) > -1;
+    },
+
+    // Write code to remove duplicates from an unsorted linked list
+    // FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
+    removeDuplicatesFromUnsortedLinkedList: function () {
+        // it is the same as:
+        // removeDuplicateCharactersHashTable
+        // removeDuplicateCharactersWithoutAdditionalMemory
     }
 };
--- JavaScript/test/c1Spec.js
+++ JavaScript/test/c1Spec.js
@@ -1,25 +1,15 @@
-describe('isStringHasAllUniqueCharacters', function() {
-    var invalidArgumentError = 'Invalid argument: "str" should be a string.';
+/* global c1: true */
+var notStringValues = [undefined, null, 0, new Date(), [], {}, function () { }];
 
-    beforeEach(function() {
-        spyOn(c1, 'isStringHasAllUniqueCharacters').and.callThrough();
-    });
-
-    [undefined, null, 0, new Date(), [], {}, function() {}].forEach(function(str) {
-        it('Invalid argument: throws error', function() {
-            expect(function() {
-                c1.isStringHasAllUniqueCharacters(str);
-            }).toThrowError(invalidArgumentError);
+describe('isStringHasAllUniqueCharacters', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument, expects false', function () {
+            expect(c1.isStringHasAllUniqueCharacters(str)).toBe(false);
         });
     });
 
-
-    it('empty string expects true', function() {
-        // empty string
+    it('tests', function () {
         expect(c1.isStringHasAllUniqueCharacters('')).toBe(true);
-    });
-
-    it('tests', function() {
         expect(c1.isStringHasAllUniqueCharacters('a')).toBe(true);
         expect(c1.isStringHasAllUniqueCharacters('ab')).toBe(true);
         expect(c1.isStringHasAllUniqueCharacters('abcdefghijklmnopqrstuvwxyz')).toBe(true);
@@ -28,22 +18,14 @@ describe('isStringHasAllUniqueCharacters', function() {
     });
 });
 
-describe('reverseString', function() {
-    var invalidArgumentError = 'Invalid argument: "str" should be a string.';
-
-    beforeEach(function() {
-        spyOn(c1, 'reverseString').and.callThrough();
-    });
-
-    [undefined, null, 0, new Date(), [], {}, function() {}].forEach(function(str) {
-        it('Invalid argument: throws error', function() {
-            expect(function() {
-                c1.reverseString(str);
-            }).toThrowError(invalidArgumentError);
+describe('reverseString', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument: expects to do nothing', function () {
+            expect(c1.reverseString(str)).toBe(str);
         });
     });
 
-    it('tests', function() {
+    it('tests', function () {
         expect(c1.reverseString('')).toEqual('');
         expect(c1.reverseString('a')).toEqual('a');
         expect(c1.reverseString('ab')).toEqual('ba');
@@ -52,22 +34,14 @@ describe('reverseString', function() {
     });
 });
 
-describe('removeDuplicateCharactersWithoutAdditionalMemory', function() {
-    var invalidArgumentError = 'Invalid argument: "str" should be a string.';
-
-    beforeEach(function() {
-        spyOn(c1, 'removeDuplicateCharactersWithoutAdditionalMemory').and.callThrough();
-    });
-
-    [undefined, null, 0, new Date(), [], {}, function() {}].forEach(function(str) {
-        it('Invalid argument: throws error', function() {
-            expect(function() {
-                c1.removeDuplicateCharactersWithoutAdditionalMemory(str);
-            }).toThrowError(invalidArgumentError);
+describe('removeDuplicateCharactersWithoutAdditionalMemory', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument: expects do nothing', function () {
+            expect(c1.removeDuplicateCharactersWithoutAdditionalMemory(str)).toBe(str);
         });
     });
 
-    it('tests', function() {
+    it('tests', function () {
         expect(c1.removeDuplicateCharactersWithoutAdditionalMemory('')).toEqual('');
         expect(c1.removeDuplicateCharactersWithoutAdditionalMemory('a')).toEqual('a');
         expect(c1.removeDuplicateCharactersWithoutAdditionalMemory('ab')).toEqual('ab');
@@ -79,22 +53,14 @@ describe('removeDuplicateCharactersWithoutAdditionalMemory', function() {
     });
 });
 
-describe('removeDuplicateCharactersHashTable', function() {
-    var invalidArgumentError = 'Invalid argument: "str" should be a string.';
-
-    beforeEach(function() {
-        spyOn(c1, 'removeDuplicateCharactersHashTable').and.callThrough();
-    });
-
-    [undefined, null, 0, new Date(), [], {}, function() {}].forEach(function(str) {
-        it('Invalid argument: throws error', function() {
-            expect(function() {
-                c1.removeDuplicateCharactersHashTable(str);
-            }).toThrowError(invalidArgumentError);
+describe('removeDuplicateCharactersHashTable', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument: expects do nothing', function () {
+            expect(c1.removeDuplicateCharactersHashTable(str)).toBe(str);
         });
     });
 
-    it('tests', function() {
+    it('tests', function () {
         expect(c1.removeDuplicateCharactersHashTable('')).toEqual('');
         expect(c1.removeDuplicateCharactersHashTable('a')).toEqual('a');
         expect(c1.removeDuplicateCharactersHashTable('ab')).toEqual('ab');
@@ -103,5 +69,227 @@ describe('removeDuplicateCharactersHashTable', function() {
         expect(c1.removeDuplicateCharactersHashTable('aaa')).toEqual('a');
         expect(c1.removeDuplicateCharactersHashTable('aaabbb')).toEqual('ab');
         expect(c1.removeDuplicateCharactersHashTable('abcdefghijklmnopqrstuvwxyza')).toEqual('abcdefghijklmnopqrstuvwxyz');
+    });
+});
+
+describe('areAnagramsXor', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument', function () {
+            expect(c1.areAnagramsXor(str, str)).toBe(false);
+            expect(c1.areAnagramsXor('normal string', str)).toBe(false);
+            expect(c1.areAnagramsXor(str, 'normal string')).toBe(false);
+        });
+    });
+
+    it('tests', function () {
+        expect(c1.areAnagramsXor('', '')).toBe(false);
+        expect(c1.areAnagramsXor('1', '')).toBe(false);
+        expect(c1.areAnagramsXor('', '1')).toBe(false);
+
+        expect(c1.areAnagramsXor('a', 'a')).toBe(true);
+        expect(c1.areAnagramsXor('ab', 'ab')).toBe(true);
+        expect(c1.areAnagramsXor('ab', 'ba')).toBe(true);
+        expect(c1.areAnagramsXor('abc', 'acb')).toBe(true);
+        expect(c1.areAnagramsXor('abc', 'cba')).toBe(true);
+        expect(c1.areAnagramsXor('abcdefghijklmnopqrstuvwxyz', 'zyxwvutsrqponmlkjihgfedcba')).toBe(true);
+        expect(c1.areAnagramsXor('abcdefghijklmnopqrstuvwxyzc', 'zyxwvutsrqponmlkjihgfedcbaa')).toBe(false);
+    });
+});
+
+describe('replaceWhiteSpaceWith', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument', function () {
+            expect(c1.replaceWhiteSpaceWith(str, str)).toBe(str);
+            expect(c1.replaceWhiteSpaceWith('normal string', str)).toEqual('normal string');
+            expect(c1.replaceWhiteSpaceWith(str, 'normal string')).toBe(str);
+        });
+    });
+
+    it('tests', function () {
+        expect(c1.replaceWhiteSpaceWith('', '')).toEqual('');
+        expect(c1.replaceWhiteSpaceWith('1', '')).toEqual('1');
+        expect(c1.replaceWhiteSpaceWith('', '1')).toEqual('');
+
+        expect(c1.replaceWhiteSpaceWith('a b', 'z')).toBe('azb');
+        expect(c1.replaceWhiteSpaceWith(' a b', 'z')).toBe('zazb');
+        expect(c1.replaceWhiteSpaceWith('a b ', 'z')).toBe('azbz');
+        expect(c1.replaceWhiteSpaceWith(' a b ', 'z')).toBe('zazbz');
+    });
+});
+
+describe('rotateMatrix90Degrees', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument', function () {
+            expect(c1.rotateMatrix90Degrees(str)).toBe(str);
+        });
+    });
+
+    it('tests', function () {
+        expect(c1.rotateMatrix90Degrees([1, 3])).toEqual([
+            [1],
+            [3]
+        ]);
+        expect(c1.rotateMatrix90Degrees([
+            [1],
+            [3]
+        ])).toEqual([
+            [3, 1]
+        ]);
+
+        expect(c1.rotateMatrix90Degrees([
+            [1, 2],
+            [3, 4]
+        ])).toEqual([
+            [3, 1],
+            [4, 2]
+        ]);
+
+        expect(c1.rotateMatrix90Degrees([
+            [1, 2, 3],
+            [4, 5, 6]
+        ])).toEqual([
+            [4, 1],
+            [5, 2],
+            [6, 3]
+        ]);
+
+        expect(c1.rotateMatrix90Degrees([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ])).toEqual([
+            [7, 4, 1],
+            [8, 5, 2],
+            [9, 6, 3]
+        ]);
+
+        expect(c1.rotateMatrix90Degrees([
+            [1, 2, 3, 4],
+            [6, 7, 8, 9],
+            [10, 11, 12, 13]
+        ])).toEqual([
+            [10, 6, 1],
+            [11, 7, 2],
+            [12, 8, 3],
+            [13, 9, 4]
+        ]);
+
+        expect(c1.rotateMatrix90Degrees([
+            [1, 2, 3, 4],
+            [6, 7, 8, 9],
+            [10, 11, 12, 13],
+            [13, 14, 15, 16]
+        ])).toEqual([
+            [13, 10, 6, 1],
+            [14, 11, 7, 2],
+            [15, 12, 8, 3],
+            [16, 13, 9, 4]
+        ]);
+    });
+});
+
+describe('setMatrixZero', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument', function () {
+            expect(c1.setMatrixZero(str)).toBe(str);
+        });
+    });
+
+    it('tests', function () {
+        expect(c1.setMatrixZero([1, 3])).toEqual([1, 3]);
+        expect(c1.setMatrixZero([
+            [1],
+            [3]
+        ])).toEqual([
+            [1],
+            [3]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [0],
+            [3]
+        ])).toEqual([
+            [0],
+            [0]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1],
+            [0]
+        ])).toEqual([
+            [0],
+            [0]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1, 2],
+            [0, 4]
+        ])).toEqual([
+            [0, 2],
+            [0, 0]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1, 0, 3],
+            [4, 5, 6]
+        ])).toEqual([
+            [0, 0, 0],
+            [4, 0, 6]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1, 2, 3],
+            [4, 0, 6],
+            [7, 8, 9]
+        ])).toEqual([
+            [1, 0, 3],
+            [0, 0, 0],
+            [7, 0, 9]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1, 2, 3, 4],
+            [0, 7, 8, 9],
+            [10, 11, 12, 13]
+        ])).toEqual([
+            [0, 2, 3, 4],
+            [0, 0, 0, 0],
+            [0, 11, 12, 13]
+        ]);
+
+        expect(c1.setMatrixZero([
+            [1, 2, 3, 4],
+            [6, 0, 8, 9],
+            [10, 11, 0, 13],
+            [13, 14, 15, 16]
+        ])).toEqual([
+            [1, 0, 0, 4],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [13, 0, 0, 16]
+        ]);
+    });
+});
+
+describe('isRotationWith1CallToIsSubstring', function () {
+    notStringValues.forEach(function (str) {
+        it('Invalid argument', function () {
+            expect(c1.isRotationWith1CallToIsSubstring(str, str)).toBe(false);
+            expect(c1.isRotationWith1CallToIsSubstring('normal string', str)).toBe(false);
+            expect(c1.isRotationWith1CallToIsSubstring(str, 'normal string')).toBe(false);
+        });
+    });
+
+    it('tests', function () {
+        expect(c1.isRotationWith1CallToIsSubstring('', '')).toBe(false);
+        expect(c1.isRotationWith1CallToIsSubstring('1', '')).toBe(false);
+        expect(c1.isRotationWith1CallToIsSubstring('', '1')).toBe(false);
+
+        expect(c1.isRotationWith1CallToIsSubstring('a', 'a')).toBe(true);
+        expect(c1.isRotationWith1CallToIsSubstring('ab', 'ab')).toBe(true);
+        expect(c1.isRotationWith1CallToIsSubstring('ab', 'ba')).toBe(true);
+        expect(c1.isRotationWith1CallToIsSubstring('abc', 'cab')).toBe(true);
+        expect(c1.isRotationWith1CallToIsSubstring('abc', 'cba')).toBe(false);
+        expect(c1.isRotationWith1CallToIsSubstring('abcdefghijklmnopqrstuvwxyz', 'defghijklmnopqrstuvwxyzabc')).toBe(true);
     });
 });
